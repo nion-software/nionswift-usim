2c70c9f0e13c17d11824c0c24f89ee29aaa08be6
Chris Meyer <cmeyer1969@gmail.com>
2017-08-11 17:02:42 -0700

    Separate probe - graphic connection into ProbeView class. Tests too.

--- PlugIns/SimulatorMicroscope/test/Scan_test.py
+++ PlugIns/SimulatorMicroscope/test/Scan_test.py
@@ -1,8 +1,4 @@
-import contextlib
-import threading
-import time
 import unittest
-import numpy
 from nion.swift.model import HardwareSource
 from Scan import ScanHardwareSource
 from Scan.test import ScanControl_test
@@ -12,13 +8,22 @@ from SimulatorMicroscope import ScanDevice
 
 class TestSimulatorScan(ScanControl_test.TestScanControlClass):
 
-    def _setup_hardware_source(self) -> HardwareSource.HardwareSource:
-
-        instrument = InstrumentDevice.Instrument()
-        HardwareSource.HardwareSourceManager().register_instrument("usim_stem_controller", instrument)
+    def _setup_hardware_source(self, instrument) -> HardwareSource.HardwareSource:
         scan_adapter = ScanHardwareSource.ScanAdapter(ScanDevice.Device(instrument), "usim_scan_device", "uSim Scan")
         scan_hardware_source = ScanHardwareSource.ScanHardwareSource(scan_adapter, "usim_stem_controller")
         return scan_hardware_source
+
+    def _close_hardware_source(self) -> None:
+        pass
+
+    def _setup_instrument(self):
+        instrument = InstrumentDevice.Instrument()
+        HardwareSource.HardwareSourceManager().register_instrument("usim_stem_controller", instrument)
+        return instrument
+
+    def _close_instrument(self, instrument) -> None:
+        HardwareSource.HardwareSourceManager().unregister_instrument("usim_stem_controller")
+
 
 
 if __name__ == '__main__':
