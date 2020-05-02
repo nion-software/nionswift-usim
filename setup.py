792ed73567716b1e97fcef853e4ecbbbd961928c
Chris Meyer <cmeyer1969@gmail.com>
2020-05-01 19:01:25 -0700

    Switch to declarative setup.cfg + pyproject file. Require Python 3.7.

--- setup.py
+++ setup.py
@@ -1,29 +1,4 @@
-# -*- coding: utf-8 -*-
+from setuptools import setup
 
-"""
-To upload to PyPI, PyPI test, or a local server:
-python setup.py bdist_wheel upload -r <server_identifier>
-"""
-
-import setuptools
-import os
-
-setuptools.setup(
-    name="nionswift-usim",
-    version="0.2.1",
-    author="Nion Software",
-    author_email="swift@nion.com",
-    description="A Nion Swift package to simulate a STEM microscope, scanner, and cameras",
-    long_description=open("README.rst").read(),
-    url="https://github.com/nion-software/nionswift-usim",
-    packages=["nionswift_plugin.usim", "nionswift_plugin.usim.test"],
-    package_data={"nionswift_plugin.usim": ["manifest.json"]},
-    install_requires=['nionswift-instrumentation>=0.18.3'],
-    license='GPLv3',
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Programming Language :: Python :: 3.6",
-    ],
-    include_package_data=True,
-    python_requires='~=3.6',
-)
+if __name__ == "__main__":
+    setup()
