bdee9ad9426fa7473878348e48f0aa93692feb62
Chris Meyer <cmeyer1969@gmail.com>
2020-07-21 16:21:47 -0700

    Use new test harness in usim (2nd fix).

--- nionswift_plugin/usim/test/Scan_test.py
+++ nionswift_plugin/usim/test/Scan_test.py
@@ -1,35 +1,14 @@
 import contextlib
 import unittest
 from nion.swift import Facade
-from nion.swift.model import HardwareSource
-from nion.instrumentation import scan_base
 from nion.instrumentation.test import ScanControl_test
-from nion.utils import Registry
-from nionswift_plugin.usim import InstrumentDevice
-from nionswift_plugin.usim import ScanDevice
 
 
 class TestSimulatorScan(ScanControl_test.TestScanControlClass):
 
-    def _setup_hardware_source(self, instrument) -> HardwareSource.HardwareSource:
-        stem_controller = HardwareSource.HardwareSourceManager().get_instrument_by_id("usim_stem_controller")
-        scan_hardware_source = scan_base.ScanHardwareSource(stem_controller, ScanDevice.Device(instrument), "usim_scan_device", "uSim Scan")
-        return scan_hardware_source
-
-    def _close_hardware_source(self) -> None:
-        pass
-
-    def _setup_instrument(self):
-        instrument = InstrumentDevice.Instrument("usim_stem_controller")
-        Registry.register_component(instrument, {"stem_controller"})
-        return instrument
-
-    def _close_instrument(self, instrument) -> None:
-        HardwareSource.HardwareSourceManager().unregister_instrument("usim_stem_controller")
-
     def test_facade_record_data_with_immediate_close(self):
-        with self._make_scan_context() as scan_context:
-            document_controller, document_model, hardware_source, scan_state_controller = scan_context.objects
+        with self._test_context() as test_context:
+            hardware_source = test_context.hardware_source
             api = Facade.get_api("~1.0", "~1.0")
             hardware_source_facade = api.get_hardware_source_by_id(hardware_source.hardware_source_id, "~1.0")
             scan_frame_parameters = hardware_source_facade.get_frame_parameters_for_profile_by_index(2)
