f948eaf3eb98a9e66ab497f8bbb25dafe862a27f
Chris Meyer <cmeyer1969@gmail.com>
2020-07-21 15:46:35 -0700

    Use new test harness in usim.

--- nionswift_plugin/usim/test/Camera_test.py
+++ nionswift_plugin/usim/test/Camera_test.py
@@ -14,43 +14,9 @@ from nionswift_plugin.usim import InstrumentDevice
 
 class TestCamera(CameraControl_test.TestCameraControlClass):
 
-    def _setup_hardware_source(self, initialize: bool=True, is_eels: bool=False) -> (DocumentController.DocumentController, DocumentModel.DocumentModel, HardwareSource.HardwareSource, CameraControlPanel.CameraControlStateController):
-
-        document_model = DocumentModel.DocumentModel()
-        document_controller = DocumentController.DocumentController(self.app.ui, document_model, workspace_id="library")
-
-        # this is simulator specific. replace this code but be sure to set up self.exposure and blanked and positioned
-        # initial settings.
-        self.exposure = 0.04
-
-        instrument = InstrumentDevice.Instrument("usim_stem_controller")
-        Registry.register_component(instrument, {"stem_controller"})
-
-        camera_id = "usim_ronchigram_camera" if not is_eels else "usim_eels_camera"
-        camera_type = "ronchigram" if not is_eels else "eels"
-        camera_name = "uSim Camera"
-        camera_settings = CameraDevice.CameraSettings(camera_id)
-        camera_device = CameraDevice.Camera(camera_id, camera_type, camera_name, instrument)
-        camera_hardware_source = camera_base.CameraHardwareSource("usim_stem_controller", camera_device, camera_settings, None, None, None)
-        if is_eels:
-            camera_hardware_source.features["is_eels_camera"] = True
-            camera_hardware_source.add_channel_processor(0, HardwareSource.SumProcessor(((0.25, 0.0), (0.5, 1.0))))
-        camera_hardware_source.set_frame_parameters(0, camera_base.CameraFrameParameters({"exposure_ms": self.exposure * 1000, "binning": 2}))
-        camera_hardware_source.set_frame_parameters(1, camera_base.CameraFrameParameters({"exposure_ms": self.exposure * 1000, "binning": 2}))
-        camera_hardware_source.set_frame_parameters(2, camera_base.CameraFrameParameters({"exposure_ms": self.exposure * 1000 * 2, "binning": 1}))
-        camera_hardware_source.set_selected_profile_index(0)
-
-        HardwareSource.HardwareSourceManager().register_hardware_source(camera_hardware_source)
-
-        state_controller = CameraControlPanel.CameraControlStateController(camera_hardware_source, document_controller.queue_task, document_controller.document_model)
-        if initialize:
-            state_controller.initialize_state()
-
-        return document_controller, document_model, camera_hardware_source, state_controller
-
     def test_camera_integrate_frames_updates_frame_count_by_integration_count(self):
-        document_controller, document_model, hardware_source, state_controller = self._setup_hardware_source()
-        with contextlib.closing(document_controller), contextlib.closing(state_controller):
+        with self._test_context() as test_context:
+            hardware_source = test_context.hardware_source
             frame_parameters = hardware_source.get_frame_parameters(0)
             frame_parameters.integration_count = 4
             hardware_source.set_current_frame_parameters(frame_parameters)
@@ -64,8 +30,10 @@ class TestCamera(CameraControl_test.TestCameraControlClass):
                 hardware_source.abort_playing(sync_timeout=3.0)
 
     def test_camera_eels_works_when_it_produces_1d_data(self):
-        document_controller, document_model, hardware_source, state_controller = self._setup_hardware_source(is_eels=True)
-        with contextlib.closing(document_controller), contextlib.closing(state_controller):
+        with self._test_context(is_eels=True) as test_context:
+            document_controller = test_context.document_controller
+            document_model = test_context.document_model
+            hardware_source = test_context.hardware_source
             frame_parameters = hardware_source.get_frame_parameters(0)
             frame_parameters.binning = 256  # binning to 1d
             hardware_source.set_current_frame_parameters(frame_parameters)
