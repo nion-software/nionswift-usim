93c7ad4a2e83be7e28bee4fa334f96b889f3135a
Chris Meyer <34664+cmeyer@users.noreply.github.com>
2025-02-06 16:47:54 -0800

    Minor test restructuring to match stricter run/stop requirements.

--- nionswift_plugin/usim/__init__.py
+++ nionswift_plugin/usim/__init__.py
@@ -6,21 +6,18 @@ from nionswift_plugin.usim import InstrumentPanel
 
 def run() -> None:
     acquisition_context_configuration = DeviceConfiguration.AcquisitionContextConfiguration()
-
     Registry.register_component(acquisition_context_configuration.instrument, {"instrument_controller", "stem_controller"})
-
     component_types = {"camera_module"}  # the set of component types that this component represents
-
     setattr(acquisition_context_configuration.ronchigram_camera_device, "camera_panel_type", "ronchigram")
     Registry.register_component(CameraDevice.CameraModule("usim_stem_controller", acquisition_context_configuration._ronchigram_camera_device, acquisition_context_configuration._ronchigram_camera_settings), component_types)
-
     setattr(acquisition_context_configuration.eels_camera_device, "camera_panel_type", "eels")
     Registry.register_component(CameraDevice.CameraModule("usim_stem_controller", acquisition_context_configuration._eels_camera_device, acquisition_context_configuration._eels_camera_settings), component_types)
-
     Registry.register_component(acquisition_context_configuration.scan_module, {"scan_module"})
-
     InstrumentPanel.run(acquisition_context_configuration._instrument)
 
 
 def stop() -> None:
+    for component in Registry.get_components_by_type("camera_module"):
+        Registry.unregister_component(component, {"camera_module"})
     Registry.unregister_component(Registry.get_component("scan_module"), {"scan_module"})
+    Registry.unregister_component(Registry.get_component("stem_controller"), {"instrument_controller", "stem_controller"})
